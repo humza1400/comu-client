3a145c982001a66cfb9d08480f99d1e772a3fe6e
player <humza484@gmail.com>
2025-06-04 17:21:56 -0400

    Initial Commit

---
+++ src/main/java/me/comu/Client.java
@@ -0,0 +1,10 @@
+package me.comu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
